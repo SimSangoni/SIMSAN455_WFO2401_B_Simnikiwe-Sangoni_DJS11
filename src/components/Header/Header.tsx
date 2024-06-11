681b46cc6c7a483bd965acbd2fda779bc755aa07
Simnikiwe Sangoni <simnikiwesangoni@gmail.com>
2024-06-11 20:06:46 +0200

    introduced toggle menu state and toggle menu function

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,25 +1,33 @@
 import { Link } from "react-router-dom";
 import './Header.css';
+import { useState } from "react";
 
 interface HeaderProps {
     title: string;
 }
 
 export default function Header({title}: HeaderProps){
-return (
-    <header className="header">
-        <div className="header-content">
-            <div className="logo">
-                <Link to='/'>{title}</Link> 
+
+    const [menuOpen, setMenuOpen] = useState(false)
+
+    function toggleMenu() {
+        setMenuOpen(!menuOpen)
+    }
+
+    return (
+        <header className="header">
+            <div className="header-content">
+                <div className="logo">
+                    <Link to='/'>{title}</Link> 
+                </div>
+                <nav className="nav">
+                    <ul className="nav-links">
+                        <li><Link to='/'>Home</Link></li>
+                        <li><Link to='/shows'>Shows</Link></li>
+                        <li><Link to='/genres'>Genres</Link></li>
+                    </ul>
+                </nav>
             </div>
-            <nav className="nav">
-                <ul className="nav-links">
-                    <li><Link to='/'>Home</Link></li>
-                    <li><Link to='/shows'>Shows</Link></li>
-                    <li><Link to='/genres'>Genres</Link></li>
-                </ul>
-            </nav>
-        </div>
-    </header>
-)
+        </header>
+    )
 }
