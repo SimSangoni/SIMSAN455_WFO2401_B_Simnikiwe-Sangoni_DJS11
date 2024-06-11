ac3e3aa62ab0606f716464828ef1346add3723f9
Simnikiwe Sangoni <simnikiwesangoni@gmail.com>
2024-06-11 21:39:09 +0200

    added icons for eveeything, tjeme commented out so long, will deal with it later.

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,8 +3,12 @@ import './Header.css';
 import { useState } from "react";
 
 // Import Icons for header page
-
 import HomeIcon from './header-icons/home-icon.svg'
+import ShowsIcon from './header-icons/show-icon.svg'
+import GenresIcon from './header-icons/genre-icon.svg'
+import SearchIcon from './header-icons/search-icon.svg'
+import UserIcon from './header-icons/user-icon.svg'
+import MenuIcon from './header-icons/menu-icon.svg'
 
 interface HeaderProps {
     title: string;
@@ -29,25 +33,48 @@ export default function Header({title}: HeaderProps){
                     <ul className="nav-links">
                         <li><Link to='/'>
                                 <span className="nav-item">
-                                <img src={HomeIcon} alt="Home" className="nav-icon" />
-                                    Home
-                                </span>
-                                
+                                    <img src={HomeIcon} alt="Home" className="nav-icon" />
+                                        Home
+                                </span>   
                             </Link>
                         </li>
-                        <li><Link to='/shows'>Shows</Link></li>
-                        <li><Link to='/genres'>Genres</Link></li>
+                        <li>
+                            <Link to='/shows'> 
+                                <span className="nav-item">
+                                    <img src={ShowsIcon} alt="Shows" className="nav-icon" />
+                                        Shows
+                                </span>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to='/genres'>
+                                <span className="nav-item">
+                                    <img src={GenresIcon} alt="Genres" className="nav-icon" />
+                                    Genres
+                                </span>              
+                            </Link>
+                        </li>
+                        {/* <li>
+                            <span className="nav-item">
+                                Theme
+                            </span>
+                        </li> */}
                     </ul>
                 </nav>
 
                 <div className="menu-toggle" onClick={toggleMenu}>
-                    <span>Home</span>
+                    <span>
+                        <img src={MenuIcon} alt="Menu" className="nav-icon" />
+                    </span>
                 </div>
             
                 <div className="user-actions">
-                    <span className="search-icon">Search</span>
-                    <span>Login</span>
-                    <button>Toggle Theme</button>
+                    <span className="search-icon">
+                        <img src={SearchIcon} alt="Search" className="actions-icon" />
+                    </span>
+                    <span>
+                        <img src={UserIcon} alt="User" className="actions-icon" />
+                    </span>
                 </div>
             </div>
         </header>
