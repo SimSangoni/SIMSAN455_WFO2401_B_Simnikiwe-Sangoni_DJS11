e6beae28b3f49c6b375a35d28565b804aa445f06
Simnikiwe Sangoni <simnikiwesangoni@gmail.com>
2024-06-11 17:17:39 +0200

    basic header components

---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+
+
+export default function Header(){
+    return (
+        <header>
+            <div>
+                Header Title
+            </div>
+            <nav>
+
+            </nav>
+        </header>
+    )
+}
