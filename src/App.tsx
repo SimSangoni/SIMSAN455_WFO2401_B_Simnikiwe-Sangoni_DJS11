ddc7f8cf8504adda964da18a8a735db446c60bf7
Simnikiwe Sangoni <simnikiwesangoni@gmail.com>
2024-06-11 19:10:05 +0200

    imported the neccesary pages and installed react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,8 @@
-import Header from "./components/Header/Header"
+import Header from "./components/Header/Header";
+import Home from "./pages/Home/Home";
+import Shows from "./pages/Shows/Shows";
+import Genres from "./pages/Genres/Genres";
+
 
 export default function App() {
 
