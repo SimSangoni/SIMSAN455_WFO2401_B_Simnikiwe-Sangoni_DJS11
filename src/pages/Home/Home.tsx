e22e45b054eeb8a938c40e573f6969c9a995ca12
Simnikiwe Sangoni <simnikiwesangoni@gmail.com>
2024-06-12 14:05:36 +0200

    fetched the shows from the api

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,18 @@
 
 export default function Home(){
+
+    async function fetchShows() {
+        try {
+          const response = await fetch('https://podcast-api.netlify.app/');
+          const data = await response.json();
+          console.log(data[0]); 
+        } catch (error) {
+          console.error('Error fetching shows:', error);
+        }
+      };
+
+      fetchShows()
+
     return (
         
             <div>
