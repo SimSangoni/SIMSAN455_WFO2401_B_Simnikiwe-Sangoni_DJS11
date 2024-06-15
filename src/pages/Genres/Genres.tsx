2362135e2e71a53bc1ec4e65f3b27b0846ae0af8
Simnikiwe Sangoni <simnikiwesangoni@gmail.com>
2024-06-15 13:30:58 +0200

    intro to states for the genres and interfaces to be used.

--- src/pages/Genres/Genres.tsx
+++ src/pages/Genres/Genres.tsx
@@ -1,5 +1,33 @@
+import { useState, useEffect } from "react";
+
+interface Show {
+    id: string;
+    title: string;
+    description: string;
+    seasons: number;
+    image: string;
+    genres: number[];
+    updated: string;
+  }
+
+  interface Genre {
+    id: number;
+    title: string;
+    description: string;
+    shows: string[];
+  }
 
 export default function Genres(){
+    const [genres, setGenres] = useState<Genre[]>([]);
+    const [shows, setShows] = useState<Show[]>([]);
+    const [selectedGenre, setSelectedGenre] = useState<Genre | null>(null);
+    const [loading, setLoading] = useState(true);
+
+    
+
+
+
+
     return (
         
             <div>
