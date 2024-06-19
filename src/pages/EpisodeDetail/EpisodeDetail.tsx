8c53a4028d1ae064d67df92b33c32bcce51808bd
Simnikiwe Sangoni <simnikiwesangoni@gmail.com>
2024-06-19 13:50:00 +0200

    audio for selected episode...

--- src/pages/EpisodeDetail/EpisodeDetail.tsx
+++ src/pages/EpisodeDetail/EpisodeDetail.tsx
@@ -1,11 +1,19 @@
-
-
+import { useLocation } from "react-router-dom";
+import { Episode } from "../../utils/Interfaces";
 
 export default function EpisodeDetail(){
+
+const location = useLocation();
+  const { episode } = location.state as { episode: Episode };
+
     return (
         
-            <div>
-                This is the episodes page
+            <div className="episode-detail">
+                <h2>{episode.title}</h2>
+                <audio controls>
+                    <source src={episode.file} type="audio/mpeg" />
+                    Your browser does not support the audio element.
+                </audio>
             </div> 
     )
 }
