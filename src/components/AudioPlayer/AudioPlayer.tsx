1e2909bbd09abae2f1a666f9d8073916b48843e2
Simnikiwe Sangoni <simnikiwesangoni@gmail.com>
2024-06-19 16:44:42 +0200

    added a bit more styling for the player functionality

--- src/components/AudioPlayer/AudioPlayer.tsx
+++ src/components/AudioPlayer/AudioPlayer.tsx
@@ -45,23 +45,25 @@ export default function AudioPlayer(){
         <div className="audio-player">
             <div className="audio-player-content">
                 <div className="audio-player-info">
-                    <h3 className='episode-title'>{episode.title}</h3>
-                    {/* <p className='episode-description'>{episode.description}</p> */}
+                    <div className='episode-details'>
+                        <h3 className='episode-title'>{episode.title}</h3>
+                        <p className='episode-description'>{episode.description}</p>
+                    </div>
                 </div>
                 <audio ref={audioRef} controls>
                     <source src={episode.file} type="audio/mpeg" />
                     Your browser does not support the audio element.
                 </audio>
-            </div>
-            <div className="controls">
-                    <button onClick={playPrevEpisode}>Previous</button>
-                    <button onClick={handleRewind}>Rewind 10s</button>
-                    <button onClick={handleFastForward}>Fast Forward 10s</button>
+                <div className="controls">
+                    <button onClick={playPrevEpisode}>Prev</button>
+                    <button onClick={handleRewind}>-10s</button>
+                    <button onClick={handleFastForward}>+10s</button>
                     <button onClick={playNextEpisode}>Next</button>
                     <button onClick={toggleShuffle}>{isShuffling ? 'Disable Shuffle' : 'Enable Shuffle'}</button>
                     <button onClick={toggleRepeat}>{isRepeating ? 'Disable Repeat' : 'Enable Repeat'}</button>
                     <button onClick={toggleFavorite}>{isFavorite ? 'Unfavorite' : 'Favorite'}</button>
                 </div>
+            </div>
         </div>
     )
 }
