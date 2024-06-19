fe6a7b2580015ed553c406442144c4816e48ac35
Simnikiwe Sangoni <simnikiwesangoni@gmail.com>
2024-06-19 14:55:38 +0200

    define state for the playEpisode and import necesary interfaces for TypeScript

--- src/components/AudioPlayer/AudioPlayerContext.tsx
+++ src/components/AudioPlayer/AudioPlayerContext.tsx
@@ -1,6 +1,18 @@
-
-import { createContext } from "react";
-import { AudioPlayerContextProps } from "../../utils/Interfaces";
+import { createContext, useState } from "react";
+import { AudioPlayerContextProps, Episode } from "../../utils/Interfaces";
 
 const AudioPlayerContext = createContext<AudioPlayerContextProps | undefined>(undefined)
 
+export function AudioPlayerProvider(){
+    const [episode, setEpisode] = useState<Episode | null>(null);
+
+    const playEpisode = (episode: Episode) => {
+        setEpisode(episode);
+    }
+
+    return (
+        <div>
+            This is the AudioPlayerProvider
+        </div>
+    )
+}
